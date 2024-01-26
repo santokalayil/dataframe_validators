f2f1999482d8516d966dd9adcb329fd3901037b5
Santo K Thomas <santokalayil@Santos-MacBook-Air.local>
2024-01-26 22:28:51 +0530

    some minor changes

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -5,25 +5,26 @@ from pydantic import (
     EmailStr,
     Field,
     field_validator,
-    constr,
+    conlist,
 )
 
 AgeInt: TypeAlias = Annotated[int, Field(strict=False, gt=0)]
 # ValidAgeCasted: TypeAlias = Annotated[int, Field(strict=False, gt=0)]
 WeightFloat: TypeAlias = Annotated[float, Field(strict=True, ge=15, le=100)]
-StudentId: TypeAlias = Annotated[str, Field(strict=True, min_length=6, max_length=6)]
-IdStr: TypeAlias = Annotated[str, Field(strict=True, pattern=r"^\d{4}$")]
+StudentId: TypeAlias = Annotated[str, Field(strict=False, min_length=6, max_length=6)]
+IdStr: TypeAlias = Annotated[str, Field(strict=False, pattern=r"^\d{4}$")]
+InterestList: TypeAlias = conlist(item_type=str, min_length=2)  # type: ignore
 
 
 class Student(BaseModel):
     id: IdStr  # Field(strict=True, pattern=r"^\d{4}$")
     # ^ start of string $ end of string. but it should contain only numbers. len should be 4 digits
-    student_id: StudentId  # type: ignore
+    student_id: StudentId
     name: str
     age: AgeInt
     weight: WeightFloat
     email: EmailStr
-    interests: list[str]
+    interests: InterestList
 
     class Config:
         use_enum_values = True
@@ -36,6 +37,10 @@ class Student(BaseModel):
     @field_validator("interests", mode="before")
     def split_interests(cls, value):
         return map(str.upper, value.split(",")) if isinstance(value, str) else value
+
+    # @field_validator("id", mode="after")
+    # def convert_id_2_str(cls, value):
+    #     return str(value) if isinstance(value, int) else value
 
     # @field_validator()
 
